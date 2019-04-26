d9d49a6f65c2f7057a61eef3384e91db73fee725
Sen Li <LS.LearnShare@gmail.com>
2019-04-26 01:31:43 -0700

    formatter

---
+++ includes/ft_formatter.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_formatter.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evilcat <evilcat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/25 23:05:02 by evilcat           #+#    #+#             */
+/*   Updated: 2019/04/26 00:17:52 by evilcat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FORMATTER_H
+#define FT_FORMATTER_H
+
+#include "ft_struct.h"
+#include "ft_read_input.h"
+#include "ft_specifier.h"
+
+#endif
