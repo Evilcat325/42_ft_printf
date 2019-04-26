acc7d1173031c86af529dbccfc0c64022be183dc
Sen Li <LS.LearnShare@gmail.com>
2019-04-26 02:32:40 -0700

    first iteration

--- src/ft_specifier.c
+++ src/ft_specifier.c
@@ -6,7 +6,7 @@
 /*   By: evilcat <evilcat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 14:50:19 by seli              #+#    #+#             */
-/*   Updated: 2019/04/26 01:30:36 by evilcat          ###   ########.fr       */
+/*   Updated: 2019/04/26 02:32:29 by evilcat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,12 @@ char *ft_spec_b(t_state_t *s)
 	return result;
 }
 
-char *ft_spec_o(t_state_t *s);
+char *ft_spec_o(t_state_t *s)
 {
 	char *result;
 
 	result = ft_ltoa_base(s->fmt.input.u, 8);
+	return result;
 }
 
 char *ft_spec_u(t_state_t *s)
@@ -52,10 +53,67 @@ char *ft_spec_x(t_state_t *s)
 	return result;
 }
 
-char *ft_spec_f(t_state_t *s);
-char *ft_spec_e(t_state_t *s);
-char *ft_spec_g(t_state_t *s);
-char *ft_spec_a(t_state_t *s);
+char *ft_spec_f(t_state_t *s)
+{
+	char *result;
+
+	result = ft_dtoa(s->fmt.input.f, s->fmt.percision);
+	return result;
+}
+
+static int	ft_get_exponent(long double d, long double *dst)
+{
+	int e;
+	e = 0;
+	while (d >= 10 && (d /= 10))
+		e++;
+	while (d < 1 && (d *= 10))
+		e--;
+	if (dst)
+		*dst = d;
+	return (e);
+}
+
+char *ft_spec_e(t_state_t *s)
+{
+	char		*result;
+	long double	d;
+	int			e;
+
+	e = ft_get_exponent(s->fmt.input.f, &d);
+	result = ft_dtoa(d, s->fmt.percision);
+	result = ft_strjoin_free(result, ft_format_sci(e), TRUE, TRUE);
+	return result;
+}
+
+char *ft_spec_g(t_state_t *s)
+{
+	char		*result_f;
+	char		*result_e;
+	long double d;
+	int	 		e;
+
+	d = s->fmt.input.f;
+	e = ft_get_exponent(s->fmt.input.f, &d);
+	s->fmt.percision -= e + 1;
+	result_f = ft_spec_f(s);
+	result_e = ft_spec_e(s);
+	if (ft_strlen(result_f) <= ft_strlen(e))
+	{
+		free(result_e);
+		return (result_f);
+	}
+	else
+	{
+		 free(result_f);
+		 return (result_e);
+	}
+}
+
+char *ft_spec_a(t_state_t *s)
+{
+	return NULL;
+}
 
 char *ft_spec_c(t_state_t *s)
 {
